1f574ecb339bba1131b0271d7fac7f5cc22c7f81
naionel63 <naionel63@gmail.com>
2023-01-30 04:50:52 -0300

    Initial commit

---
+++ Js/Js.js
@@ -0,0 +1,42 @@
+function enNegro1() {
+    Element =document.getElementById("Me");
+    Element.src="img/Me2.png";
+    }
+    function enBlanco1() {
+        Element =document.getElementById("Me");
+        Element.src="img/Me.png";
+    }
+    function enNegro2() {
+        Element =document.getElementById("Stdy");
+        Element.src="img/Study2.png";
+    }
+    function enBlanco2() {
+        Element =document.getElementById("Stdy");
+        Element.src="img/Study.png";
+    }
+    function enNegro3() {
+        Element =document.getElementById("Wo");
+        Element.src="img/Work2.png";
+    }
+     function enBlanco3() {
+        Element =document.getElementById("Wo");
+        Element.src="img/Work.png";
+    }
+    
+    function enNegro4() {
+        Element =document.getElementById("Sk");
+        Element.src="img/Skills2.png";
+    }
+    function enBlanco4() {
+        Element =document.getElementById("Sk");
+        Element.src="img/Skills.png";
+    }
+    function enNegro5() {
+        Element =document.getElementById("Cntc");
+        Element.src="img/Contact2.png";
+    }
+    function enBlanco5() {
+        Element =document.getElementById("Cntc");
+        Element.src="img/Contact.png";
+    }
+    
